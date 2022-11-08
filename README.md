2e5efbb98136e35af33518bfe1f2c452b476e918
mgetz34 <mgetz340@gmail.com>
2022-11-07 18:54:06 -0700

    added js html and css files, as well as bootstrap and jquery to base code

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Weather-Dashboard-
+# Weather-Dashboard
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## Table of Contents (Optional)
+
+## Installation
+
+Deploy the application via the URL below
+
+## Usage
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+## License
+
+Refer to the license in the repo.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+## Application URL
+
+[Weather Dashboard](https://)
---
+++ assets/script.js
@@ -0,0 +1,23 @@
+// User Story
+
+// AS A traveler, I WANT to see the weather outlook for multiple cities
+// SO THAT I can plan a trip accordingly
+
+// Acceptance Criteria 
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5 - day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+var openWeatherAPI = "71943e874f3a60eaa03270f17d4ac3ea";
+var city;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather";
+
+fetch(queryURL)
