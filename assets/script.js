9296c9725b17672ef8f06e35a246d662bacc542f
mgetz34 <mgetz340@gmail.com>
2022-11-12 15:49:31 -0700

    added to js

--- assets/script.js
+++ assets/script.js
@@ -93,9 +93,10 @@ function renderCards(forecastFiveday) {
     for (let index = 0; index < forecastFiveday.length; index++) {
         const element = forecastFiveday[index];
         console.log(element);
+        var weather = document.createElement = ("div");
         var card = document.createElement = ("div");
+        forecastContainer.append(weather);
         forecastContainer.append(card);
-        card.append(data.dt_txt);
     }
 
 }
